64d8f21dcef18d298ed935bc7c8edc398070a6bd
Krishna Kasyap <125851796+kasyap2807@users.noreply.github.com>
2024-11-25 14:37:23 +0530

    Uploading Spring FIles Final

---
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Model.Users;
+import com.example.demo.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+
+    @PostMapping("/register")
+    public Users register(@RequestBody Users user) {
+        return service.register(user);
+
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody Users user) {
+
+        return service.verify(user);
+    }
+}
